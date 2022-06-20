7c549d96f65841118a895501f6cc5723ded73805
Jason Gornall <jason.gornall@sourcegraph.com>
2022-06-20 10:14:25 -0700

    some debugging and a general loader flow

--- dev/actions/functions/src/index.ts
+++ dev/actions/functions/src/index.ts
@@ -1,6 +1,7 @@
 import { App, ExpressReceiver } from '@slack/bolt'
 import * as functions from 'firebase-functions'
 import axios from 'axios'
+import { BlockAction } from "@slack/bolt/dist/types/actions/block-action"
 
 export interface PriorityItem {
     value: string
@@ -31,22 +32,158 @@ const app = new App({
     token: config.bot.slack_bot_token as string,
 })
 
-app.command('/jason-testing', async ({ ack, say }) => {
+app.command('/jason-testing', async ({ ack, say, client, body }) => {
     try {
         await ack()
-        say('Yaaay! that command works!')
+        await say({
+            "blocks": [
+                {
+                    "type": "section",
+                    "text": {
+                        "type": "mrkdwn",
+                        "text": "This is a section block with a button."
+                    },
+                    "accessory": {
+                        "type": "button",
+                        "text": {
+                            "type": "plain_text",
+                            "text": "Click Me"
+                        },
+                        "value": "click_me_123",
+                        "action_id": "button"
+                    }
+                }
+            ]
+        })
     } catch (error) {
         console.log('err')
         console.error(error)
     }
 })
 
-app.action('button', async ({ ack, say, action }) => {
-    await ack()
-    await say('Yaaay! that command works!')
+app.view('view_1', async ({ ack, body, view, client, logger, payload, respond }) => {
+    // Acknowledge the view_submission request
+    await ack();
+  
+    console.log(JSON.stringify(payload, null, 2))
+    // Do whatever you want with the input data - here we're saving it to a DB then sending the user a verifcation of their submission
+  
+    // Assume there's an input block with `block_1` as the block_id and `input_a`
+    const user = body['user']['id'];
+    // Message to send user
+    let msg = '';
+    // Save to DB  
+    if (true) {
+      // DB save was successful
+      msg = 'Your submission was successful';
+    } else {
+      msg = 'There was an error with your submission';
+    }
+  
+    // Message the user
+    try {
+        axios.post(payload.private_metadata, {
+            "blocks": [
+                {
+                    "type": "section",
+                    "text": {
+                        "type": "mrkdwn",
+                        "text": "WAKKA WAKKA." + Date.now()
+                    },
+                    "accessory": {
+                        "type": "button",
+                        "text": {
+                            "type": "plain_text",
+                            "text": "Click Me"
+                        },
+                        "value": "click_me_123",
+                        "action_id": "button"
+                    }
+                }
+            ]
+        })
+        await client.chat.postMessage({
+            channel: user,
+            text: msg
+        });
+    }
+    catch (error) {
+      logger.error(error);
+    }
+  
+  });
+
+
+app.action('button', async ({ ack, say, action, client, body }) => {
+    try {
+        await ack()
+        
+        const result = await client.views.open({
+            // Pass a valid trigger_id within 3 seconds of receiving it
+            trigger_id: (<BlockAction>body).trigger_id,
+            // View payload
+            view: {
+              type: 'modal',
+              // View identifier
+              callback_id: 'view_1',
+              private_metadata: (<BlockAction>body).response_url,
+              title: {
+                type: 'plain_text',
+                text: 'Modal title'
+              },
+              blocks: [
+                {
+                  type: 'section',
+                  text: {
+                    type: 'mrkdwn',
+                    text: 'Welcome to a modal with _blocks_'
+                  },
+                  accessory: {
+                    type: 'button',
+                    text: {
+                      type: 'plain_text',
+                      text: 'Click me!'
+                    },
+                    action_id: 'button_abc'
+                  }
+                },
+                {
+                  type: 'input',
+                  block_id: 'input_c',
+                  label: {
+                    type: 'plain_text',
+                    text: 'What are your hopes and dreams?'
+                  },
+                  element: {
+                    type: 'plain_text_input',
+                    action_id: 'dreamy_input',
+                    multiline: true
+                  }
+                }
+              ],
+              submit: {
+                type: 'plain_text',
+                text: 'Submit',
+              }
+            }
+          });
+    } catch (error) {
+        console.log('err')
+        console.error(error)
+    }
     // Update the message to reflect the action
 })
-
+const generateLoader = (body: any) => {
+    let clone = JSON.parse(JSON.stringify(body))
+    const selects = clone.message.blocks.filter(
+        (block: any) => !!(block.accessory && block.accessory.type == 'static_select')
+    )
+    selects.forEach(element => {
+        delete element.accessory;
+        element.text.text += ' :loading:'
+    });
+    return { blocks: clone.message.blocks};
+}
 const updateUrlIfChanged = async (issueNumber: string, channelId: string, messageTs: string) => {
     const issue = await axios.get(`https://api.github.com/repos/sourcegraph/sourcegraph/issues/${issueNumber}`, {
         headers: {
@@ -84,10 +221,10 @@ app.action('priority_select', async ({ ack, say, action: actionBase, respond, bo
         (block: any) => block.accessory && block.accessory.action_id === action.action_id
     )
     let message = relevantBlock.text.text
+
+    // generate loading
     message = message.replace(/\n.+/, '')
-    
-    relevantBlock.text.text = message + '\n :loading:'
-    await respond({ blocks: body.message.blocks })
+    await respond((generateLoader(body)))
 
     const timestamp = `\`<!date^${parseInt(
         action.action_ts,
@@ -167,8 +304,7 @@ app.action('estimate_select', async ({ ack, say, action: actionBase, respond, bo
     let message = relevantBlock.text.text
     message = message.replace(/\n.+/, '')
 
-    relevantBlock.text.text = message + '\n :loading:'
-    await respond({ blocks: body.message.blocks })
+    await respond((generateLoader(body)))
 
     const timestamp = `\`<!date^${parseInt(
         action.action_ts,
